151f3b9063e7cf2aec1feb3ebd0f9cb2b7aa2400
mqz <gmarquez@witapp.com.ar>
2017-02-18 00:42:07 -0300

    Fix perfil edit on user form

--- src/SeguridadBundle/Form/UsuarioAdminType.php
+++ src/SeguridadBundle/Form/UsuarioAdminType.php
@@ -29,7 +29,7 @@ class UsuarioAdminType extends AbstractType
                                         'class' => 'SeguridadBundle:Perfil',
                                         'query_builder' => function (EntityRepository $er) use($user) {
                                             $query = $er->createQueryBuilder('p');
-                                            //if(!$user->hasRole('ROLE_ADMIN'))
+                                            if(!$user->hasRole('ROLE_ADMIN'))
                                                 $query->where('p.availableForNewUsers = 1');
                                             return $query->orderBy('p.name', 'ASC');
                                         },
