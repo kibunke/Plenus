b0bfa55d916ecf1f31826f674197d2a7bdeaea86
mqz <gmarquez@witapp.com.ar>
2017-03-29 23:50:51 -0300

    fixed up delegate new Equipos Class on Planilla

--- src/InscripcionBundle/Controller/SegmentoController.php
+++ src/InscripcionBundle/Controller/SegmentoController.php
@@ -61,10 +61,16 @@ class SegmentoController extends Controller
                     "data"            => array(),
                     "state"           => array('actives' => $filter['actives'], 'inactives' => $filter['total']-$filter['actives'])
         );
-        
+        $logger = $this->get('logger');
         foreach ($filter['rows'] as $segmento){
             $inscriptos = $em->getRepository('InscripcionBundle:Segmento')->getTotalInscriptos($segmento,$this->getUser());
+            $logger->info('############');
+            $logger->info(var_dump($inscriptos));
+            $logger->info('############');
             $inscriptos = $segmento->getTotalInscriptosFromQuery($inscriptos);
+            $logger->info('############');
+            $logger->info(var_dump($inscriptos));
+            $logger->info('############');
             $data['data'][] = array(
                 "id"        => $segmento->getId(),
                 "segmento"  => $segmento->getNombreCompletoRaw(),
